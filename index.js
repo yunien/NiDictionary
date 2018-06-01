6498ab7e5fce1629dde3617d33f9759276221bc6
yunien_lin <yunien_lin@wistron.com>
2018-06-01 10:07:54 +0800

    add error handle

--- index.js
+++ index.js
@@ -24,7 +24,8 @@ const readFile = (fileName) => {
     const jsonString = fs.readFileSync(filePath, 'utf8');
     return JSON.parse(jsonString);
   }else{
-    console.log(filePath, ' 檔案不存在。');
+    const errMsg = filePath+'檔案不存在。';
+    throw new Error(errMsg);
   }
 };
 
@@ -34,7 +35,8 @@ const writeFile = async (fileName, nweEnString) => {
   if(fs.existsSync(filePath)){
     await fs.writeFileSync(filePath, nweEnString);
   }else{
-    console.log(filePath, ' 檔案不存在。');
+    const errMsg = filePath+'檔案不存在。';
+    throw new Error(errMsg);
   }
 };
 
@@ -58,16 +60,20 @@ const findDatas = async (fileJson) => {
 };
 
 app.get('/findData/:fileName', async (req, res) => {
-  const fileName = req.params.fileName;
-  const fileJson = await readFile(fileName);
-  const datas = await findDatas(fileJson);
-  res.send({catalogs: datas});
+  try {
+    const fileName = req.params.fileName;
+    const fileJson = await readFile(fileName);
+    const datas = await findDatas(fileJson);
+    res.send({catalogs: datas});
+  } catch (error) {
+    res.status(403).send({errorMsg: error.message});
+  }
 });
 
 const findREaddir = () => new Promise((rs, rj) => {
   fs.readdir('./public', (err, files) => {
     if (err) {
-      console.log(err);
+      rj(err);
       return;
     }
     let fileArray = [];
@@ -82,50 +88,58 @@ const findREaddir = () => new Promise((rs, rj) => {
 });
 
 app.get('/getFileName', async (req, res) => {
-  findREaddir().then(result => {
-    res.send({files: result});
-  }).catch(err => {throw err});
+  findREaddir()
+    .then(result => {
+      res.send({files: result});
+    })
+    .catch(
+      error => {res.status(403).send({errorMsg: error.message})}
+    );
 });
 
 app.post('/add/:fileName', async (req, res) => {
-  const fileName = req.params.fileName;
-  
-  const enJson = await readFile(fileName);
-  
-  const data = {[`${req.body.twData}`]: req.body.cnData};
-
-  Object.assign(enJson, data);
-  const nweEnString = JSON.stringify(enJson);
-  await writeFile(fileName, nweEnString);
-
-  //  有變數，自動創建檔案，檔案名：變數名稱
-  let resultValue = '';
-  const splitContexts = req.body.twData.split('$');
-  if(splitContexts.length > 1){ //有變數
-    forEach(splitContexts, (item, index, arr) => {
-      const start = item.indexOf('{');
-        const end = item.indexOf('}', start+1);
-        const key = item.substring(start+1, end);
-        if (key !== ''){
-          createFile(key);
-        }
-      });
+  try {
+    const fileName = req.params.fileName;
+    const enJson = await readFile(fileName);
+    const data = {[`${req.body.twData}`]: req.body.cnData};
+    Object.assign(enJson, data);
+    const nweEnString = JSON.stringify(enJson);
+    await writeFile(fileName, nweEnString);
+    //  有變數，自動創建檔案，檔案名：變數名稱
+    let resultValue = '';
+    const splitContexts = req.body.twData.split('$');
+    if(splitContexts.length > 1){ //有變數
+      forEach(splitContexts, (item, index, arr) => {
+        const start = item.indexOf('{');
+          const end = item.indexOf('}', start+1);
+          const key = item.substring(start+1, end);
+          if (key !== ''){
+            createFile(key);
+          }
+        });
+    }
+    res.status(200).send({twData: req.body.twData, cnData: req.body.cnData});
+  } catch (error) {
+    res.status(403).send({errorMsg: error.message});
   }
-  res.status(200).send({twData: req.body.twData, cnData: req.body.cnData});
 });
 
 app.post('/remove/:fileName', async (req, res) => {
-  const fileName = req.params.fileName;
-  console.log('remove');
-  const enJson = await readFile(fileName);
-  const twData = req.body.twData;
+  try {
+    const fileName = req.params.fileName;
+    console.log('remove');
+    const enJson = await readFile(fileName);
+    const twData = req.body.twData;
+    
+    delete enJson[twData];
   
-  delete enJson[twData];
-
-  const nweEnString = JSON.stringify(enJson);
-  await writeFile(fileName, nweEnString);
-
-  res.status(200).send({twData: req.body.twData});
+    const nweEnString = JSON.stringify(enJson);
+    await writeFile(fileName, nweEnString);
+  
+    res.status(200).send({twData: req.body.twData});
+  } catch (error) {
+    res.status(403).send({errorMsg: error.message});
+  }
 });
 
 const getVariable = (element) => {
@@ -136,44 +150,46 @@ const getVariable = (element) => {
 };
 
 app.post('/conveter', async (req, res) => {
-  const {fileName, textKey, textBody} = req.body;
-  const textKeyArr = textKey.split('$');
-  const textBodyArr = textBody.split('$');
-  let mappingArr = textKeyArr.map((d, i) => {
-    if(i > 0) {
-      const fName = getVariable(textKeyArr[i]);
-      const fKey = getVariable(textBodyArr[i]);
-      return {fName, fKey};
-    } else {
-      return {fName: 'base', fKey: textKey};
-    }
-  });
-
-  const baseJson = readFile(fileName);
-  const firstValue = baseJson[textKey];
-  const firstValueArr = firstValue.split('$');
-  const resultArr = firstValueArr.map((d, i) => {
-    if (i === 0) {
-      return d;
-    } else {
-      const secondFileName = getVariable(d);
-      const json = readFile(secondFileName);
-      const mappingObj = mappingArr.find(d => d.fName === secondFileName);
-      const secondValue = json[mappingObj.fKey];
-      if(secondValue === undefined){
-        return d.replace('{'+secondFileName+'}', 'OOO');
-      }else{
-        return d.replace('{'+secondFileName+'}', secondValue);
+  try {
+    const {fileName, textKey, textBody} = req.body;
+    const textKeyArr = textKey.split('$');
+    const textBodyArr = textBody.split('$');
+    let mappingArr = textKeyArr.map((d, i) => {
+      if(i > 0) {
+        const fName = getVariable(textKeyArr[i]);
+        const fKey = getVariable(textBodyArr[i]);
+        return {fName, fKey};
+      } else {
+        return {fName: 'base', fKey: textKey};
       }
-    }
-  });
-  
-  let result = '';
-  resultArr.forEach(item => {
-    result = result + item;
-  });
-
-  res.send({value: result});  
+    });
+    const baseJson = readFile(fileName);
+    const firstValue = baseJson[textKey];
+    const firstValueArr = firstValue.split('$');
+    const resultArr = firstValueArr.map((d, i) => {
+      if (i === 0) {
+        return d;
+      } else {
+        const secondFileName = getVariable(d);
+        const json = readFile(secondFileName);
+        const mappingObj = mappingArr.find(d => d.fName === secondFileName);
+        const secondValue = json[mappingObj.fKey];
+        if(secondValue === undefined){
+          return d.replace('{'+secondFileName+'}', 'OOO');
+        }else{
+          return d.replace('{'+secondFileName+'}', secondValue);
+        }
+      }
+    });
+    
+    let result = '';
+    resultArr.forEach(item => {
+      result = result + item;
+    });
+    res.send({value: result});
+  } catch (error) {
+    res.status(403).send({errorMsg: error.message});
+  }
 });
 
 process.on('unhandledRejection', error => {
